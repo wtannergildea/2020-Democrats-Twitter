32632a7e7e477f0fdf6f50654cc0dd75518089e4
Tanner Gildea <tanner@dhcp-10-250-106-58.harvard.edu>
2019-05-07 13:40:33 -0400

    Almost done commenting.

--- 2020-Dems/app.R
+++ 2020-Dems/app.R
@@ -1,3 +1,5 @@
+# Even though loaded in the script, still necessary to load libraries here too.
+
 library(shiny)
 library(twitteR)
 library(ggplot2)
@@ -20,9 +22,11 @@ nrc_tweets <- read_rds("nrc_tweets")
 bing_tweets <- read_rds("bing_tweets")
 word_freq <- read_rds("word_frequency")
 
-# For word freq by candidate section
-kamala_words <- read_rds("kamala_words")
+# For word freq by candidate section, if necessary in the future (unused now)
+# kamala_words <- read_rds("kamala_words")
 
+# I make some small edits to the cleaned summary table created in the script - need
+# to rename the columns.
 
 summary_table <- read_rds("clean_summary_table") %>% 
   rename("Handle" = "screenName", 
@@ -35,7 +39,8 @@ summary_table <- read_rds("clean_summary_table") %>%
 # USER INTERFACE
 ####################################
 
-# Define UI for application that draws a histogram
+# Define UI for the application, including a navigation bar at the top.
+
 ui <- navbarPage("2020 Dems on Twitter", theme = shinytheme("flatly"),
                  
 ####################################                 
@@ -47,12 +52,17 @@ ui <- navbarPage("2020 Dems on Twitter", theme = shinytheme("flatly"),
   fluidPage(
    
    # Application title
+    
    titlePanel("The Story of the 2020 Democratic Primary, as Told Through Twitter"),
+   
+   # The p(paste("text")) function is how I insert text into the app, repeated many times.
    
    p(paste("A record number of Democrats are seeking their party's nomination for the Presidency
           in 2020. Presumptive front-runners Joe Biden and Bernie Sanders aren't scaring away the
           competition: the field includes  former and sitting U.S. senators, representatives, 
           governors, a mayor, cabinet secretaries, an author, and a tech executive.")),
+   
+   # The br() function adds white space to the app.
    
    br(),
    
@@ -89,7 +99,10 @@ tabPanel("Summary Statistics",
          fluidPage(
            
          # Application title
+           
            titlePanel("Summary of Twitter Activity"),
+           
+           # This sidebar allows for the user to control the bin width of the visualization.
            
            sidebarLayout(
              sidebarPanel(
@@ -100,23 +113,20 @@ tabPanel("Summary Statistics",
                            value = 30), 
                width = 2),
              
-             # checkboxInput("candidate",
-             #                    "KamalaHarris",
-             #               value = TRUE),
-               
                  
-              # Show a plot of the generated distribution
+              # The main panel will feature text and the two visualizations for this tab.
+             
               mainPanel(
                 
                 "Not all candidates have taken to Twitter with the same frequency as others.
                 Here you can see the candidates' Twitter activity since the beginning of 2019, as visualized
                 by the volume of their tweets over time.",
                 
-                # I want to add another input control that allows the user to change the data range,
-                # in most cases make it smaller. 
+                br(),
+                br(),
                 
-                br(),
-                br(),
+                # This function pulls the created visualization from the section below, and outputs
+                # it in the app.
                 
                 plotOutput("tweet_freq"),
                 
@@ -134,6 +144,8 @@ tabPanel("Summary Statistics",
                 
                 br(),
                 
+                # The DT package is very helpful for making interactive tables.
+                
                 DTOutput("summary_table"),
                 
                 br(),
@@ -150,24 +162,8 @@ tabPanel("Sentiment Analysis",
          fluidPage(
            
            # Application title
-           titlePanel("Sentiment Analysis"),
            
-           # Sidebar with a slider input for number of bins 
-           # sidebarLayout(
-          
-          # If this works, would allows the user to toggle between types of lexicons. May not be necessary.
-             
-             #   sidebarPanel(
-             #     selectInput("tweet_sentiments", "Choose Sentiment:",
-             #                 c("Average Positivity: Afinn",
-             #                   "Positivity and Negativity: Bing",
-             #                   "Positivity and Negativity: NRC"),
-             #                 "Average Positivity: Afinn") # sets default
-             # ),
-             
-             # Show a plot of the generated distribution
-             # mainPanel(
-               #put in the plotlyOutput function here
+           titlePanel("Sentiment Analysis"),
                
                p(paste("Using a methodology called Sentiment Analysis, we are able to get a better
                sense of the tone of each candidate. This is done by checking each candidate's tweets
@@ -183,7 +179,10 @@ tabPanel("Sentiment Analysis",
                        of their negativity rating. What outliers do you notice among the candidates?")),
           
                br(),
-          
+               
+               # I created visualizations for all three lexicons in the script, so I only need to
+               # pull them from below here and stick them in between my text.
+           
                plotlyOutput("afinn"),
                
                br(),
@@ -211,7 +210,6 @@ tabPanel("Sentiment Analysis",
                
                plotlyOutput("bing")
                
-               # text description of the visualization above
                
              )
            ),
@@ -225,11 +223,16 @@ tabPanel("Individual Key Words",
          fluidPage(
            
            # Application title
+           
            titlePanel("Key Words"),
            
-           # Sidebar with a slider input for number of bins 
+           # Sidebar with multiple inputs:
+           
            sidebarLayout(
              sidebarPanel(
+               
+               # This text input box allows the user to search for any keyword within each
+               # candidate's tweets. 
                
                textInput("word", "Please enter your keyword, e.g. healthcare.", "healthcare"),
                
@@ -254,6 +257,9 @@ tabPanel("Individual Key Words",
                br(),
                br(),
                
+               # This drop-down box allows the user to select a particular candidate to view,
+               # within the visualization of their top 25 used words.
+               
                selectInput("candidate", 
                            "Please select a candidate to view.",
                            c("KamalaHarris",
@@ -270,7 +276,9 @@ tabPanel("Individual Key Words",
                            "SenGillibrand"))
              ),
              
-             # Show a plot of the generated distribution
+             # Now for the main text and visualizations, which change according to the selected
+             # inputs above.
+             
              mainPanel(
                
                p(paste("Finally, it is also interesting to analyze candidates' language at the individual word
@@ -299,6 +307,7 @@ tabPanel("Individual Key Words",
                
                # I could also make a visualization of word use between two candidates the user selects,
                # like Mara did here: https://github.com/batpigandme/tidymueller/
+               # This is something to be considered in a future project.
              )
            )
          )),
@@ -311,7 +320,8 @@ tabPanel("Footnotes",
          
          fluidPage(
            
-           # Application title
+           # I also want to add an acknowledgements page at the end.
+           
            titlePanel("Acknowledgements"),
            
            p(paste("Although I worked on this project alone, I could not have done it alone.")),
@@ -320,8 +330,10 @@ tabPanel("Footnotes",
            
            p(paste("I'd like to thank Michael Galarnyk, who wrote an excellent Medium article
                    explaining how to access Twitter's API using R.")),
-
-          p(paste("Check it out here: https://medium.com/@GalarnykMichael/accessing-data-from-twitter-api-using-r-part1-b387a1c7d3e")),
+          
+          # Still trying to figure out how to make this a hyperlink using HTML tags.
+          
+           p(paste("Check it out here: https://medium.com/@GalarnykMichael/accessing-data-from-twitter-api-using-r-part1-b387a1c7d3e")),
            
            br(),
            
@@ -331,12 +343,7 @@ tabPanel("Footnotes",
            br(),
            
            p(paste("And finally, I give my full gratitude to David Kane (Preceptor), the head of Gov:1005 (Data) at Harvard and the
-                  rope that protected me from the sirens.")),
-           
-           # Show a plot of the generated distribution
-           mainPanel(
-             # insert here eventually
-           )
+                  rope that protected me from the sirens."))
          )))
 
 ###################################
@@ -344,7 +351,9 @@ tabPanel("Footnotes",
 ###################################
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to create all the visualizations above. This is what's 
+# "under the hood" of the app itself.
+
 server <- function(input, output) {
 
   ###################################
@@ -355,9 +364,6 @@ server <- function(input, output) {
    output$tweet_freq <- renderPlot({
      
      cleaned_tweets %>% 
-       
-     # filter(screenName == input$candidate) %>% 
-       
      ggplot(aes(x = created, fill = screenName)) +
        
        geom_histogram(
@@ -389,6 +395,9 @@ server <- function(input, output) {
      output$summary_table <- renderDT(
        
        summary_table,
+       
+       # This are additional attributes particular to the DT package.
+       
        class = 'display', 
        rownames = FALSE,
        options = list(dom = 't')
@@ -400,7 +409,6 @@ server <- function(input, output) {
    ####################################
    
    
-
    output$afinn <- renderPlotly(
        
     # AFINN OUTPUT
@@ -413,7 +421,7 @@ server <- function(input, output) {
             fill = "Select Candidate") +
        theme(axis.title.x=element_blank(),
              # axis.text.x=element_blank(),
-             axis.text.x = element_text(angle = 60, hjust = 1))
+             axis.text.x = element_text(angle = 60, hjust = 1)) # very useful/cool for angling 
              # axis.ticks.x=element_blank())
      )
      
@@ -456,6 +464,9 @@ server <- function(input, output) {
    
    output$word_count_table <- renderDT(
      
+  # This is for creating the DT word frequency table. It also connects to the user's input
+  # as they select for which word they want to search.
+     
      word_freq %>% dplyr::filter(Word == input$word),
      class = 'display',
      rownames = FALSE,
@@ -463,17 +474,21 @@ server <- function(input, output) {
      
    )
    
+  # This creates the visualization of the top 25 words per candidate. The original code
+  # is in the script, but I found it easier to do it all here.
+   
    output$candidate_words <- renderPlot({
     
      candidate_words <- 
        cleaned_tweets %>% 
-       filter(screenName == input$candidate) %>%
+       filter(screenName == input$candidate) %>% # Allows for selection of particular candidate.
        unnest_tokens(word, text) %>% 
        anti_join(stop_words) %>% 
        count(word) %>% 
-       filter(word != "https" & word != "t.co") %>% 
+       filter(word != "https" & word != "t.co") %>% # A few filler words kept appearing
        arrange(desc(n)) %>% 
-       slice(1:25) 
+       slice(1:25) # Takes top 25 only
+     
      
        ggplot(candidate_words, aes(x = reorder(word, n), y = n)) +
        geom_col() +
